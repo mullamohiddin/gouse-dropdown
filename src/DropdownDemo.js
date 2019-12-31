e7f87c21f44f6d4d7604ba8afb92d83b926f2d3c
Gouse <mgousem@gmail.com>
2019-12-31 09:00:54 +0400

    chanegs added

--- src/DropdownDemo.js
+++ src/DropdownDemo.js
@@ -1,32 +1,193 @@
 import React, {Component} from 'react';
-import {Dropdown} from 'primereact/dropdown';
+import {Dropdown} from './Dropdown/Dropdown';
 import {TieredMenu} from 'primereact/tieredmenu';
+import {InputText} from 'primereact/inputtext';
 import './App.css';
 import styled from "styled-components";
-
+import * as _ from 'lodash'
 export class DropdownDemo extends Component {
+    data = [
+      {
+         label:'File',
+         icon:'pi pi-fw pi-file',
+         items:[
+            {
+               label:'New',
+               icon:'pi pi-fw pi-plus',
+               items:[
+                  {
+                     label:'Bookmark',
+                     icon:'pi pi-fw pi-bookmark',
+                     command:(e)=>{ this.onCarChange2(e)},
+                  },
+                  {
+                     label:'Video',
+                     icon:'pi pi-fw pi-video',
+                     command:(e)=>{ this.onCarChange2(e)},
+                  },
+   
+               ]
+            },
+            {
+               label:'Delete',
+               icon:'pi pi-fw pi-trash',
+               command:(e)=>{ this.onCarChange2(e)}
+            },
+            {
+               label:'Export',
+               icon:'pi pi-fw pi-external-link',
+               command:(e)=>{ this.onCarChange2(e)}
+            }
+         ]
+      },
+      {
+         label:'Edit',
+         icon:'pi pi-fw pi-pencil',
+         items:[
+            {
+               label:'Left',
+               icon:'pi pi-fw pi-align-left',
+               command:(e)=>{ this.onCarChange2(e)}
+            },
+            {
+               label:'Right',
+               icon:'pi pi-fw pi-align-right',
+               command:(e)=>{ this.onCarChange2(e)}
+            },
+            {
+               label:'Center',
+               icon:'pi pi-fw pi-align-center',
+               command:(e)=>{ this.onCarChange2(e)}
+            },
+            {
+               label:'Justify',
+               icon:'pi pi-fw pi-align-justify',
+               command:(e)=>{ this.onCarChange2(e)}
+            },
+   
+         ]
+      },
+      {
+         label:'Users',
+         icon:'pi pi-fw pi-user',
+         items:[
+            {
+               label:'New',
+               icon:'pi pi-fw pi-user-plus',
+               command:(e)=>{ this.onCarChange2(e)}
+   
+            },
+            {
+               label:'Delete',
+               icon:'pi pi-fw pi-user-minus',
+               command:(e)=>{ this.onCarChange2(e)},
+   
+            },
+            {
+               label:'Search',
+               icon:'pi pi-fw pi-users',
+               items:[
+                  {
+                     label:'Filter',
+                     icon:'pi pi-fw pi-filter',
+                     items:[
+                        {
+                           label:'Print',
+                           icon:'pi pi-fw pi-print',
+                           command:(e)=>{ this.onCarChange2(e)},
+                        }
+                     ]
+                  },
+                  {
+                     icon:'pi pi-fw pi-bars',
+                     label:'List',
+                     command:(e)=>{ this.onCarChange2(e)},
+                  }
+               ]
+            }
+         ]
+      },
+      {
+         label:'Events',
+         icon:'pi pi-fw pi-calendar',
+         items:[
+            {
+               label:'Edit',
+               icon:'pi pi-fw pi-pencil',
+               items:[
+                  {
+                     label:'Save',
+                     icon:'pi pi-fw pi-calendar-plus',
+                     command:(e)=>{ this.onCarChange2(e)},
+                  },
+                  {
+                     label:'Delete',
+                     icon:'pi pi-fw pi-calendar-minus',
+                     command:(e)=>{ this.onCarChange2(e)},
+                  },
+   
+               ]
+            },
+            {
+               label:'Archieve',
+               icon:'pi pi-fw pi-calendar-times',
+               items:[
+                  {
+                     label:'Remove',
+                     icon:'pi pi-fw pi-calendar-minus',
+                     command:(e)=>{ this.onCarChange2(e)},
+                  }
+               ]
+            }
+         ]
+      },
+      {
+         label:'Quit',
+         icon:'pi pi-fw pi-power-off',
+         command:(e)=>{ this.onCarChange2(e)},
+      }
+  ];
     constructor() {
         super();
         this.state = {
             car2: 'BMW',
-            car: null, 
+            car: null,
+            items: this.data
         }
         this.onCarChange2 = this.onCarChange2.bind(this);
     }
  
-  
+    customFilter(val) {
+       val = val instanceof Object ? [val] : val;
+    var f =  this.filterTree(this.state.filter, val);
+       return f;
+    }
 
+    componentDidMount() {
+    this.setState({items: this.data});
+  }
+ filterTree = (filter, list) =>  {
+  return _.filter(list, (item) => {
+    if (item.items && item.items.length >0 ) {
+      item.items = this.filterTree(filter, item.items);
+      return !_.isEmpty(item.items);
+    } else if (_.includes(_.toLower(item.label), _.toLower(filter))) {
+      return true;
+    }
+  });
+};
       onCarChange2(e) {
         this.setState({car2: e.item.label});
     }
-    onChangeDropdown(event) {
-      
+        setGlobal(e) {
+         const data = this.filterTree(e.target.value, this.state.items)
+         this.setState({items: [...data]});
         }
     carTemplate(option) {
-const optionArray = [option]
+const optionArray = option instanceof Object ? [option] : option
             return (
               
-                <TieredMenu model={optionArray}>
+                <TieredMenu  model={optionArray}>
             
                  </TieredMenu>
   
@@ -35,147 +196,6 @@ const optionArray = [option]
     }
 
     render() {
-        const items = [
-            {
-               label:'File',
-               icon:'pi pi-fw pi-file',
-               items:[
-                  {
-                     label:'New',
-                     icon:'pi pi-fw pi-plus',
-                     items:[
-                        {
-                           label:'Bookmark',
-                           icon:'pi pi-fw pi-bookmark',
-                           command:(e)=>{ this.onCarChange2(e)},
-                        },
-                        {
-                           label:'Video',
-                           icon:'pi pi-fw pi-video',
-                           command:(e)=>{ this.onCarChange2(e)},
-                        },
-         
-                     ]
-                  },
-                  {
-                     label:'Delete',
-                     icon:'pi pi-fw pi-trash',
-                     command:(e)=>{ this.onCarChange2(e)}
-                  },
-                  {
-                     label:'Export',
-                     icon:'pi pi-fw pi-external-link',
-                     command:(e)=>{ this.onCarChange2(e)}
-                  }
-               ]
-            },
-            {
-               label:'Edit',
-               icon:'pi pi-fw pi-pencil',
-               items:[
-                  {
-                     label:'Left',
-                     icon:'pi pi-fw pi-align-left',
-                     command:(e)=>{ this.onCarChange2(e)}
-                  },
-                  {
-                     label:'Right',
-                     icon:'pi pi-fw pi-align-right',
-                     command:(e)=>{ this.onCarChange2(e)}
-                  },
-                  {
-                     label:'Center',
-                     icon:'pi pi-fw pi-align-center',
-                     command:(e)=>{ this.onCarChange2(e)}
-                  },
-                  {
-                     label:'Justify',
-                     icon:'pi pi-fw pi-align-justify',
-                     command:(e)=>{ this.onCarChange2(e)}
-                  },
-         
-               ]
-            },
-            {
-               label:'Users',
-               icon:'pi pi-fw pi-user',
-               items:[
-                  {
-                     label:'New',
-                     icon:'pi pi-fw pi-user-plus',
-                     command:(e)=>{ this.onCarChange2(e)}
-         
-                  },
-                  {
-                     label:'Delete',
-                     icon:'pi pi-fw pi-user-minus',
-                     command:(e)=>{ this.onCarChange2(e)},
-         
-                  },
-                  {
-                     label:'Search',
-                     icon:'pi pi-fw pi-users',
-                     items:[
-                        {
-                           label:'Filter',
-                           icon:'pi pi-fw pi-filter',
-                           items:[
-                              {
-                                 label:'Print',
-                                 icon:'pi pi-fw pi-print',
-                                 command:(e)=>{ this.onCarChange2(e)},
-                              }
-                           ]
-                        },
-                        {
-                           icon:'pi pi-fw pi-bars',
-                           label:'List',
-                           command:(e)=>{ this.onCarChange2(e)},
-                        }
-                     ]
-                  }
-               ]
-            },
-            {
-               label:'Events',
-               icon:'pi pi-fw pi-calendar',
-               items:[
-                  {
-                     label:'Edit',
-                     icon:'pi pi-fw pi-pencil',
-                     items:[
-                        {
-                           label:'Save',
-                           icon:'pi pi-fw pi-calendar-plus',
-                           command:(e)=>{ this.onCarChange2(e)},
-                        },
-                        {
-                           label:'Delete',
-                           icon:'pi pi-fw pi-calendar-minus',
-                           command:(e)=>{ this.onCarChange2(e)},
-                        },
-         
-                     ]
-                  },
-                  {
-                     label:'Archieve',
-                     icon:'pi pi-fw pi-calendar-times',
-                     items:[
-                        {
-                           label:'Remove',
-                           icon:'pi pi-fw pi-calendar-minus',
-                           command:(e)=>{ this.onCarChange2(e)},
-                        }
-                     ]
-                  }
-               ]
-            },
-            {
-               label:'Quit',
-               icon:'pi pi-fw pi-power-off',
-               command:(e)=>{ this.onCarChange2(e)},
-            }
-        ];
         
 const classNames = {
     container: 'p-tieredmenu',
@@ -239,13 +259,16 @@ const AutosuggestWrapper = styled.div.attrs(classNames)`
    font-size: 12px;
  }
     }
-
+   
 `
-        return (
+     
+return (
             <AutosuggestWrapper className="my-style">
                <div  className={this.props.className}>
-                   <Dropdown  placeholder="Please Select"  value={this.state.car2} options={items} itemTemplate={this.carTemplate}
-                    filter={true} filterPlaceholder="Select Val" filterBy="label" showClear={true}
+                   <Dropdown  placeholder="Please Select"  value={this.state.car2} options={this.state.items} itemTemplate={this.carTemplate}
+                    filterMatchMode="custom" 
+                    filterElement={this.setGlobal}
+                    filter={true} filterPlaceholder="Select Val" filterBy={this.customFilter}  showClear={true}
                                >
                     
                               </Dropdown>
